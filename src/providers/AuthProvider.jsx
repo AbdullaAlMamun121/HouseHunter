4bac793e4b6da08f314f750195bc1342895aaa66
codestation0 <codestation2023@gmail.com>
2023-07-17 16:58:06 +0600

    first commit

---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,61 @@
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile } from "firebase/auth";
+import app from '../firebase/firebase.config';
+
+const auth = getAuth(app);
+
+export const AuthContext = createContext(null)
+const AuthProvider = ({ children }) => {
+    // user and loading state
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+    // create new user
+    const createUser = (email, password) => {
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+    // sing in user methods
+    const signIn = (email, password) => {
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+    // logout
+    const logOut = () => {
+        setLoading(true)
+        return signOut(auth)
+    }
+
+    // update user profile
+    const updateUserProfile = (name, photo) => {
+        return updateProfile(auth.currentUser, {
+            displayName: name,
+            photoURL: photo,
+        })
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+            console.log('current user', currentUser);
+        })
+        return () => {
+            return unsubscribe()
+        }
+    }, [])
+
+    const authInfo = {
+        user,
+        loading,
+        setLoading,
+        createUser,
+        signIn,
+        logOut,
+        updateUserProfile
+    }
+    return (
+        <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
