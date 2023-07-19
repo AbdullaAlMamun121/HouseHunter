a751aaa4ed0e11817f891967d6bd645efa129ce8
AbdullaAlMamun121 <hmdabdulla121@gmail.com>
2023-07-19 21:59:01 +0600

    added name , email, phone field when want to bokking

--- src/pages/Home/DisplayHouses/DisplayCard.jsx
+++ src/pages/Home/DisplayHouses/DisplayCard.jsx
@@ -4,86 +4,98 @@ import { AuthContext } from '../../../providers/AuthProvider';
 import { useNavigate } from 'react-router-dom';
 import { saveHousesInDB } from '../../../api/auth';
 
-const DisplayCard = ({houseList}) => {
-
+const DisplayCard = ({ houseList }) => {
     const [userRole, setUserRole] = useState([]);
     const [selectedItem, setSelectedItem] = useState([]);
     const [isUsers, isUsersLoading] = useAllUsers();
-    const { user,loading } = useContext(AuthContext);
+    const { user, loading } = useContext(AuthContext);
     const navigate = useNavigate();
 
     useEffect(() => {
         if (!loading && isUsers && isUsers.length > 0) {
-            const loggedInUserRole = isUsers.find(u => u.email === user?.email)?.role;
+            const loggedInUserRole = isUsers.find((u) => u.email === user?.email)?.role;
             setUserRole(loggedInUserRole ? [loggedInUserRole] : []);
         }
-    }, [loading,isUsers,user]);
+    }, [loading, isUsers, user]);
 
-    console.log(userRole);
-    const handleBooked = (item,email) => {
-        if(user){
-            setSelectedItem(item);
-            saveHousesInDB(item,email)
-        }else{
-            alert('Login first');
+    const handleBooked = () => {
+        const { name, email, phoneNumber } = user || {};
+
+        if (!name || !email || !phoneNumber) {
+            alert('Please provide your name, email, and phone number.');
+            return;
+        }
+
+        if (user) {
+            const bookingData = {
+                name: name || user.name,
+                email: email || user.email,
+                phoneNumber,
+            };
+            saveHousesInDB(houseList, bookingData);
+        } else {
+            alert('Please login first.');
             navigate('/login');
         }
-       
-    }
-    const { name,address,city,bedrooms,bathrooms,roomSize,price,availableDate,image } = houseList;
+    };
+
+    const { name, address, city, bedrooms, bathrooms, roomSize, price, availableDate, image } = houseList;
 
     return (
-        <>
-        <div className={`card shadow-xl ${availableDate === 0 ? 'bg-red-500' : 'bg-base-100'}`}>
-            <figure className="px-10 pt-10">
-                <img src={image} alt="Instructor Image" className="rounded-xl" />
-            </figure>
-            <div className="card-body items-center text-center">
-                <p>
-                    <span className="text-orange-500 capitalize">Name:</span>
-                    {name}
+        <div className="max-w-sm rounded overflow-hidden shadow-lg">
+            <img src={image} alt="House" className="w-full h-48 object-cover" />
+            <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">{name}</div>
+                <p className="text-gray-700 text-base">
+                    <span className="font-bold">Address:</span> {address}
                 </p>
-                
-                <p>
-                    <span className="text-orange-500 capitalize">Available Date:</span>
-                    {availableDate}
+                <p className="text-gray-700 text-base">
+                    <span className="font-bold">City:</span> {city}
                 </p>
-                <p>
-                    <span className="text-orange-500 capitalize">Address:</span>
-                    {address}
+                <p className="text-gray-700 text-base">
+                    <span className="font-bold">Bathrooms:</span> {bathrooms}
                 </p>
-                <p>
-                    <span className="text-orange-500 capitalize">City:</span>
-                    {city}
+                <p className="text-gray-700 text-base">
+                    <span className="font-bold">Bedrooms:</span> {bedrooms}
                 </p>
-                <p>
-                    <span className="text-orange-500 capitalize">Bathrooms:</span>
-                    {bathrooms}
+                <p className="text-gray-700 text-base">
+                    <span className="font-bold">Room Size:</span> {roomSize}
                 </p>
-                <p>
-                    <span className="text-orange-500 capitalize">Bedrooms:</span>
-                    {bedrooms}
+                <p className="text-gray-700 text-base">
+                    <span className="font-bold">Price:</span> {price}
                 </p>
-                <p>
-                    <span className="text-orange-500 capitalize">Room Size:</span>
-                    {roomSize}
+                <p className="text-gray-700 text-base">
+                    <span className="font-bold">Available Date:</span> {availableDate}
                 </p>
-                <p>
-                    <span className="text-orange-500 capitalize">Price:</span>
-                    {price}
-                </p>
-              
-
-                <button onClick={() => handleBooked(houseList,user?.email)}
-                    disabled={(userRole[0] === 'houseOwner' ||  availableDate === 0 )}
-                    className="btn btn-outline btn-accent"
+               
+                <div className="flex justify-between items-center mt-4">
+                  
+                    <div>
+                        <label htmlFor="name" className="text-gray-700 text-sm mr-2">
+                            Name:
+                        </label>
+                        <input type="text" id="name" name="name" value={user?.name || ''} disabled className="bg-gray-100 p-1 rounded" />
+                        <label htmlFor="email" className="text-gray-700 text-sm mx-2">
+                            Email:
+                        </label>
+                        <input type="email" id="email" name="email" value={user?.email || ''} disabled className="bg-gray-100 p-1 rounded" />
+                        <label htmlFor="phoneNumber" className="text-gray-700 text-sm mx-2">
+                            Phone Number:
+                        </label>
+                        <input type="tel" id="phoneNumber" name="phoneNumber" className="bg-gray-100 p-1 rounded" />
+                    </div>
+                </div>
+            </div>
+            <div className="px-6 pt-4 pb-2">
+                <button
+                    onClick={handleBooked}
+                    disabled={(userRole[0] === 'houseOwner' || availableDate === 0)}
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                 >
-                    Booking
-                   {/* {selectedItem ? 'Selected' : 'Select'} */}
+                    Book Now
                 </button>
             </div>
         </div>
-    </>
     );
 };
 
